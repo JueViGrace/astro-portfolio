7ede6845e82557ea335827412b1560360356fe7e
Daniel Zabala <danielzabalanava@gmail.com>
2024-10-21 16:29:59 -0400

    assets fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://juevigrace.github.io',
+  base: '/portfolio',
   integrations: [tailwind()],
 });
